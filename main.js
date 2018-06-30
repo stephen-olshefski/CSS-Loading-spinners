e24fea5f075b1a94e8c10cde6bddcc7a66f736a4
Stephen Olshefski <stephen.olshefski@outlook.com>
2018-06-30 19:57:57 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.main p').style.display = 'none';
+document.querySelector('.main').classList.add('spinner-3');
+
+setTimeout(() => {
+    document.querySelector('.main').classList.remove('spinner-3');
+    document.querySelector('.main p').style.display = 'block';
+}, 4000);
